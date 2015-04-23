4b8c18269d2414689b5bafc3ec5a5ce8b03cf5b0
rusell <risingdarkness@inbox.lv>
2015-04-23 14:17:20 +0300

    workflow

--- config_example.js
+++
@@ -1,8 +0,0 @@
-CONFIG = {};
-
-CONFIG.cps = {};
-CONFIG.cps.host = 'tcp://78.154.146.20:9007';
-CONFIG.cps.db = 'db_name';
-CONFIG.cps.username = 'username';
-CONFIG.cps.password = 'password';
-CONFIG.cps.account_id = 0;
